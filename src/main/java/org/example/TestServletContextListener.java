d4cf36cec2bf7329e4f18040670eef5b2dbc8488
Denis Anisimov <denis@vaadin.com>
2021-01-21 12:11:01 +0300

    Simple web project with ServletContextListener

---
+++ src/main/java/org/example/TestServletContextListener.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
+import org.slf4j.LoggerFactory;
+
+@Component(immediate = true, property = {
+        HttpWhiteboardConstants.HTTP_WHITEBOARD_LISTENER + "=true" })
+/*
+ * HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_SELECT + "=(" +
+ * HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_NAME + "=*)"
+ */
+public class TestServletContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.err.println("context is initialized");
+        LoggerFactory.getLogger(TestServletContextListener.class)
+                .error("context is initialized");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+
+}
